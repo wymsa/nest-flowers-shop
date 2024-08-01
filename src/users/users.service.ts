fea21f91b3053433b83079e1210eb432f865e176
wymsa <wymsamail@gmail.com>
2024-08-01 23:45:15 +0300

    [UPDATE] Updated user resource

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,8 +5,4 @@ import type { TEnvConfig } from '../types';
 @Injectable()
 export class UsersService {
   constructor(private readonly configService: ConfigService<TEnvConfig>) {}
-
-  hello(): string {
-    return `Hello ${this.configService.getOrThrow('DB_USERNAME')}`;
-  }
 }
